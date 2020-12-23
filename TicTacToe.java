beb5d99bea16a8c6ce2887e0b8074d4d443c49a3
sanket2498 <sanketdulange24@gmail.com>
2020-12-23 23:06:21 +0530

    Added : Ability to select letter for player and computer

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,8 +1,21 @@
+import java.util.Scanner;
+import java.util.Random;
 public class TicTacToe
 {
 	public static void main(String args[])
 	{
 		char[] board = createBoard();
+		char player,computer;
+		player=LetterSelect();
+		if(player=='X') 
+		{
+			computer='0';	
+		}
+		else
+		{
+			computer='X';
+		}
+		System.out.println("Computer Letter is:"+computer +"  "+"Player letter is:"+player);
 	}
 	private static char[] createBoard() //Creating a board method 
 	{
@@ -14,5 +27,14 @@ public class TicTacToe
 		 System.out.println(" Board Created ");
 		 return board;
 	}
+	public static char LetterSelect()//Selecting letter method
+	{
+		 System.out.println("Choose letters for player and Computer");
+		 Scanner input=new Scanner(System.in);
+		 char player;
+		 System.out.println("Enter For player(X/0)");
+		 player=input.next().charAt(0);	
+		 return player;  	 	 
+	}	
 	
 }
